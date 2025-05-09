45991698fc20ea31f4699a6da7514a38ca9d56c3
Dennis Jones <dbjlmc@gmail.com>
2025-05-08 17:10:03 -0700

    https://leetcode.com/problems/count-symmetric-integers/description/

---
+++ count-symmetric-integers/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+
+        for (int num = low; num <= high; ++num) {
+            std::string numStr = std::to_string(num);
+            int length = numStr.length();
+            if (length % 2 == 0) {
+                int halfLength = length / 2;
+                int firstHalfSum = 0;
+                int secondHalfSum = 0;
+                for (int ndx = 0; ndx < halfLength; ++ndx) {
+                    firstHalfSum += (numStr[ndx] - '0');
+                }
+                for (int ndx = halfLength; ndx < length; ++ndx) {
+                    secondHalfSum += (numStr[ndx] - '0');
+                }
+                if (firstHalfSum == secondHalfSum) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    Solution solution;
+
+    std::cout << std::format("{}", solution.countSymmetricIntegers(1, 100)) << std::endl;
+    std::cout << std::format("{}", solution.countSymmetricIntegers(1200, 1230)) << std::endl;
+    return 0;
+}
