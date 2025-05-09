5b186ec6b2aef44f173619c4ae3ad87699068e3f
Dennis Jones <dbjlmc@gmail.com>
2025-05-09 15:13:30 -0700

    removing clion auto comment

--- count-symmetric-integers/main.cpp
+++ count-symmetric-integers/main.cpp
@@ -29,7 +29,6 @@ public:
     }
 };
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     Solution solution;
 
